5d2ec7eb6f433e2012b95dd9538aaa16b0fbe077
Viktoriia Mikhailova <viktorilisenok@gmail.com>
2023-10-24 19:33:22 +0600

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from "../environments/environment";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule,
+		AngularFireAuthModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+	production: false,
+	firebaseConfig: {
+		apiKey: "AIzaSyCjyKRVT1xi1j8wsbxT4cXMMuPk22hqFXI",
+		authDomain: "good-taste-7c634.firebaseapp.com",
+		projectId: "good-taste-7c634",
+		storageBucket: "good-taste-7c634.appspot.com",
+		messagingSenderId: "1018833118189",
+		appId: "1:1018833118189:web:54837ca04f3ef80bc53de4",
+		measurementId: "G-47Z96ED99C"
+	}
+};
